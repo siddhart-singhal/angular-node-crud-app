942bb124f98cbed3f61fe6865a996fced32a0855
siddhart-singhal <siddhart.singhal@appdirect.com>
2020-04-05 16:58:59 +0530

    CRUD APP ANGULAR - INITAL COMMIT

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+let express = require('express'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    dbConfig = require('./database/db');
+
+// Connecting with mongo db
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+}).then(() => {
+        console.log('Database sucessfully connected');
+    },
+    error => {
+        console.log('Database could not connected: ' + error);
+    }
+);
+
+const employeeRoute = require('../backend/routes/employee.route');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+app.use('/api', employeeRoute);
+
+// Create port
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+    console.log('Connected to port ' + port);
+});
+
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
